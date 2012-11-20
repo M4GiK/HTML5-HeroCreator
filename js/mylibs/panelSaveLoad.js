cc38468b5a1d8964a95ca7ef68c2d9cb43188340
m4gik <michal.szczygiel@wp.pl>
2012-11-20 12:55:41 +0200

    Created easy function save

--- js/mylibs/panelSaveLoad.js
+++ js/mylibs/panelSaveLoad.js
@@ -1,1 +1,10 @@
-//M4GiK Software
+// This File conatin functions to save and load  data.
+// All data will be encapsulated to JSON string.
+// Data before load will be validate.
+//                                 ~ Michał Szczygieł
+
+function save()
+{
+    jQuery.parseJSON( jsonString );
+}
+
