82cd68c08cfeb6b380ea9559acfff28d1b669726
aleksandersmierciak <aleksander.smierciak+github@gmail.com>
2013-01-15 02:56:36 +0200

    Restructure major parts of code, both HTML and CSS
    Style character sheet layout
    Adjust SVG legend images

--- js/script.js
+++ js/script.js
@@ -2,25 +2,7 @@
 
 */
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function updateAbilityModifier(input) {
+	var modifier = Math.floor((input.value - 10) / 2);
+	document.getElementById(input.name + "_mod").value = (modifier > 0 ? "+" : "") + modifier;
+}
